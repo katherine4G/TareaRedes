e2cdc6407f834ed3769858a9d98620182ba0e044
Isaac_laptop <nachoartaviasalazar@gmail.com>
2025-05-28 10:31:26 -0600

    Final

--- emisor_local.py
+++ emisor_local.py
@@ -1,36 +1,76 @@
 # emisor_local.py
 import numpy as np
 import sounddevice as sd
+from scipy.fft import rfft, rfftfreq
 import time
 
 # Mapa de frecuencias para cada símbolo
-frecuencias_digitos = {
+frecuencias_digitos_E = {
     '0': 1000, '1': 1100, '2': 1200, '3': 1300,
     '4': 1400, '5': 1500, '6': 1600, '7': 1700,
-    '8': 1800, '9': 1900, '.': 2000, ',': 2100
+    '8': 1800, '9': 1900, '.': 2000, ',': 2100,
+    'S' :2200, 'F': 2300
+}
+
+frecuencias_digitos_R = {
+    1000: '0', 1100: '1', 1200: '2', 1300: '3',
+    1400: '4', 1500: '5', 1600: '6', 1700: '7',
+    1800: '8', 1900: '9', 2000: '.', 2100: ',',
+    2200: 'S', 2300: 'F'
 }
 
 fs = 44100  # Frecuencia de muestreo
 duracion_tono = 1  # Duración de cada tono (segundos)
 amplitud = 0.5       # Volumen
+frames_por_segmento = int(fs * duracion_tono)
+tolerancia = 40  # Hz
+
+def detectar_digito(segmento_audio):
+    fft_data = np.abs(rfft(segmento_audio))
+    freqs = rfftfreq(len(segmento_audio), 1/fs)
+    pico = np.argmax(fft_data)
+    frecuencia_detectada = freqs[pico]
+
+    for freq_ref, digito in frecuencias_digitos_R.items():
+        if abs(frecuencia_detectada - freq_ref) <= tolerancia:
+            return digito
+    return '?'
 
 def generar_tono(frecuencia, duracion):
     t = np.linspace(0, duracion, int(fs * duracion), False)
     tono = np.sin(2 * np.pi * frecuencia * t)
     return tono * amplitud
 
-mensaje = input("[EMISOR] Ingrese el mensaje a enviar (números, punto y coma): ")
+def enviarMensaje(mensaje):
+    for caracter in mensaje:
+        frecuencia = frecuencias_digitos_E.get(caracter)
+        if frecuencia:
+            tono = generar_tono(frecuencia, duracion_tono)
+            sd.play(tono, samplerate=fs)
+            sd.wait()
+            time.sleep(0.05)  # Pequeña pausa entre tonos
+        else:
+            print(f"[EMISOR] Caracter no reconocido: {caracter}")
 
-print("[EMISOR] Enviando mensaje...")
+def verificarMedio():
+    i = 0
+    try:
+        while i <= 7 :
+            grabacion = sd.rec(frames_por_segmento, samplerate=fs, channels=1, dtype='float64')
+            sd.wait()
+            segmento = grabacion[:, 0]
+            digito = detectar_digito(segmento)
+            print(f"[EMISOR] Dato detectado de alguien más: {digito}")
+            i = i + 1
+            if digito != '?':
+                i = 0
+    except KeyboardInterrupt:
+        print("\n[EMISOR] Finalizado por el usuario.")
+        print(f"[EMISOR] Mensaje completo recibido: {mensaje}")
 
-for caracter in mensaje:
-    frecuencia = frecuencias_digitos.get(caracter)
-    if frecuencia:
-        tono = generar_tono(frecuencia, duracion_tono)
-        sd.play(tono, samplerate=fs)
-        sd.wait()
-        time.sleep(0.05)  # Pequeña pausa entre tonos
-    else:
-        print(f"[EMISOR] Caracter no reconocido: {caracter}")
-
-print("[EMISOR] Mensaje enviado.")
+#---------------------------------------------------------
+id = input("[EMISOR] Ingrese el ID del nodo: ")
+while True:
+    mensaje = input("[EMISOR] Ingrese el mensaje a enviar (números, punto y coma): ")
+    verificarMedio()
+    enviarMensaje(mensaje)
--- receptor_local.py
+++ receptor_local.py
@@ -1,5 +1,4 @@
 # receptor_local.py
-# receptor_local_loop.pyr
 import numpy as np
 import sounddevice as sd
 from scipy.fft import rfft, rfftfreq
@@ -7,7 +6,8 @@ from scipy.fft import rfft, rfftfreq
 frecuencias_digitos = {
     1000: '0', 1100: '1', 1200: '2', 1300: '3',
     1400: '4', 1500: '5', 1600: '6', 1700: '7',
-    1800: '8', 1900: '9', 2000: '.', 2100: ','
+    1800: '8', 1900: '9', 2000: '.', 2100: ',',
+    2200: 'S', 2300: 'F'
 }
 
 tolerancia = 40  # Hz
@@ -15,7 +15,7 @@ fs = 44100
 duracion_tono = 1  # en segundos
 frames_por_segmento = int(fs * duracion_tono)
 
-def detectar_digito(segmento_audio):
+def interpretar_digito(segmento_audio):
     fft_data = np.abs(rfft(segmento_audio))
     freqs = rfftfreq(len(segmento_audio), 1/fs)
     pico = np.argmax(fft_data)
@@ -26,19 +26,35 @@ def detectar_digito(segmento_audio):
             return digito
     return '?'
 
-mensaje = ""
+def detectar_digito():
+    grabacion = sd.rec(frames_por_segmento, samplerate=fs, channels=1, dtype='float64')
+    sd.wait()
+    segmento = grabacion[:, 0]
+    return interpretar_digito(segmento)
+
+def recolectar_mensaje():
+    mensaje = ""
+    msgFlag = True
+
+    while msgFlag:
+        digito = interpretar_digito
+        print(f"[RECEPTOR] Dato detectado: {digito}")
+        if digito == 'F':
+            return mensaje
+        if digito != '?':
+            mensaje += digito
+
+
 
 print("[RECEPTOR] Escuchando continuamente... (Ctrl+C para detener)")
 
 try:
     while True:
-        grabacion = sd.rec(frames_por_segmento, samplerate=fs, channels=1, dtype='float64')
-        sd.wait()
-        segmento = grabacion[:, 0]
-        digito = detectar_digito(segmento)
-        if digito != '?':
-            mensaje += digito
+        digito = detectar_digito
         print(f"[RECEPTOR] Dato detectado: {digito}")
+        if digito == 'S':
+            mensaje = recolectar_mensaje
+            print(f"[RECEPTOR] trama detectada: {mensaje}")        
 except KeyboardInterrupt:
     print("\n[RECEPTOR] Finalizado por el usuario.")
     print(f"[RECEPTOR] Mensaje completo recibido: {mensaje}")
